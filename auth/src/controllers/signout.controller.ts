824638087c4a42c9cd18bbfd466d048f3d76aa14
Gautam Sharma <gautam2586272@gmail.com>
2025-02-28 14:14:08 +0530

    Added express-validator dependency and refactored user routes in the auth service. Implemented error handling middleware and a NotFoundError for unmatched routes.

---
+++ auth/src/controllers/signout.controller.ts
@@ -0,0 +1,7 @@
+import { Request, Response } from "express";
+
+const signoutController = (req: Request, res: Response) => {
+    res.send("Signout");
+}
+
+export { signoutController };
--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,12 +1,28 @@
 import express from 'express';
+import 'express-async-errors'
 import {json} from 'body-parser'
+
+import { getUserRouter } from "./routes/current-user.routes";
+import { signinRouter } from "./routes/singin.routes";
+import { signoutRouter } from "./routes/signout.routes";
+import { signupRouter } from "./routes/signup.routes";
 
 const app = express();
 app.use(json());
 
-app.get('/api/users/currentuser', (req, res) => {
-    res.send('Hi there!');
+app.use("/api/users", getUserRouter);
+app.use("/api/users", signinRouter);
+app.use("/api/users", signupRouter);
+app.use("/api/users", signoutRouter);
+
+import { errorHandler } from "./middlewares/error-handler.middleware";
+import { NotFoundError } from "./errors/not-found-error";
+
+
+app.all("*", () => {
+    throw new NotFoundError()
 })
+app.use(errorHandler);
 
 app.listen(3000, () => {
     console.log('Listening on port 3000!!!');
